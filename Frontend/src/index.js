4df7ca9f8280fd951e6a33b49a27bf78cee9d191
Yuvraj1811 <ys2029472@gmail.com>
2025-12-07 00:04:29 +0530

    change wevitals import js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 import theme from './theme';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
